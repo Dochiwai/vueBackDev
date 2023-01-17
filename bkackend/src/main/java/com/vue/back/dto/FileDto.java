77cfcccf5e310a27c5b2338aa898994be4e4aef9
Taeparu <gjsdlfkddmsd@gmail.com>
2023-01-17 15:33:54 +0900

    S3Service 설정(pom.xml추가)

--- bkackend/src/main/java/com/vue/back/dto/FileDto.java
+++ bkackend/src/main/java/com/vue/back/dto/FileDto.java
@@ -1,15 +1,26 @@
 package com.vue.back.dto;
 
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class FileDto {
 	private String uuid;
-	private String fileName;
-	private String contentType;
+	private String file_name;
+	private int file_type;
+	private long uid;
+	private long file_size;
+	private Date reg_at;
 	
 	public FileDto() {}
 	
-	public FileDto(String uuid, String fileName, String contentType) {
+	public FileDto(String uuid, String file_name, int file_type) {
 		this.uuid = uuid;
-		this.fileName = fileName;
-		this.contentType = contentType;
+		this.file_name = file_name;
+		this.file_type = file_type;
+		
 	}
 }	
